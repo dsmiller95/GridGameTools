4dd9dc17975fa0d6c7ce5ca4865d914af1fbe7a4
Dan Miller <dan.miller@fraculation.com>
2024-05-08 12:49:23 -0500

    place test tools directly in tests file

--- Assets/Packages/com.dman.grid-game-tools/Tests/Editor/TestDungeonPathingData.cs
+++ Assets/Packages/com.dman.grid-game-tools/Tests/Editor/TestDungeonPathingData.cs
@@ -1,90 +1,9 @@
-using System;
-using Dman.Math;
 using NUnit.Framework;
 using UnityEngine;
 using static GridDomain.Test.PathingDataTestHelpers;
 
 namespace GridDomain.Test
 {
-    public static class PathingDataTestHelpers
-    {
-        /// <summary> 
-        /// Will return a flags representing all chars in the string unioned together
-        /// S/N: south/north
-        /// E/W: east/west
-        /// U/D: up/dow
-        /// 
-        /// </summary>
-        /// <param name="directionChars"></param>
-        /// <returns></returns>
-        public static FacingDirectionFlags DirectionsFromString(string directionChars)
-        {
-            var result = FacingDirectionFlags.None;
-            foreach (var c in directionChars)
-            {
-                result |= c switch
-                {
-                    'N' => FacingDirectionFlags.North,
-                    'E' => FacingDirectionFlags.East,
-                    'S' => FacingDirectionFlags.South,
-                    'W' => FacingDirectionFlags.West,
-                    'U' => FacingDirectionFlags.Up,
-                    'D' => FacingDirectionFlags.Down,
-                    _ => throw new ArgumentOutOfRangeException(nameof(directionChars), directionChars, null)
-                };
-            }
-
-            return result;
-        }
-        
-        public static void AssertBlockedDirections(
-            string expectedBlockFlags,
-            IDungeonPathingData pathingData,
-            Vector3Int? stringOffset = null,
-            PathingLayers layer = PathingLayers.All)
-        {
-            stringOffset ??= Vector3Int.zero;
-            var worldBuildString = WorldBuildString.WithInlineSeparator(expectedBlockFlags, '|');
-            var xyz = worldBuildString.GetInXYZ();
-            foreach (var point in VectorUtilities.IterateAllIn(worldBuildString.Size()))
-            {
-                var directionString = xyz[point];
-                var expected = DirectionsFromString(directionString);
-                var pointInWorld = point + stringOffset.Value;
-                AssertBlocked(expected, pointInWorld);
-            }
-
-            void AssertBlocked(FacingDirectionFlags expectedFlags, Vector3Int tile)
-            {
-                Assert.AreEqual(expectedFlags, pathingData.GetFacesBlockedBiDirectional(tile, layer), $"Expected {expectedFlags} at {tile}");
-            }
-        }
-        public static void AssertBlockedByDirections(
-            string expectedBlockFlags,
-            IDungeonPathingData pathingData,
-            Vector3Int? stringOffset = null,
-            PathingLayers layer = PathingLayers.All)
-        {
-            stringOffset ??= Vector3Int.zero;
-            var worldBuildString = WorldBuildString.WithInlineSeparator(expectedBlockFlags, '|');
-            var xyz = worldBuildString.GetInXYZ();
-            foreach (var point in VectorUtilities.IterateAllIn(worldBuildString.Size()))
-            {
-                var directionString = xyz[point];
-                var expected = DirectionsFromString(directionString);
-                var pointInWorld = point + stringOffset.Value;
-                AssertBlockedBy(expected, pointInWorld);
-            }
-
-            void AssertBlockedBy(FacingDirectionFlags expectedFlags, Vector3Int tile)
-            {
-                Assert.AreEqual(expectedFlags, pathingData.GetBlockedFaces(tile, layer), $"Expected {expectedFlags} at {tile}");
-            }
-        }
-
-        
-    }
-    
     public class TestDungeonPathingData
     {
         
