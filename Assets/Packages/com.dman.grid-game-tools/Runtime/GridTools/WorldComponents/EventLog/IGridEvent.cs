a7c7b9a8fadae267146e4400528370ac49b93a8d
Dan Miller <dan.miller@fraculation.com>
2024-08-13 09:32:37 -0500

    add a non-nullable point to the grid event

--- Assets/Packages/com.dman.grid-game-tools/Runtime/GridTools/WorldComponents/EventLog/IGridEvent.cs
+++ Assets/Packages/com.dman.grid-game-tools/Runtime/GridTools/WorldComponents/EventLog/IGridEvent.cs
@@ -1,7 +1,10 @@
-﻿namespace Dman.GridGameTools.EventLog
+﻿using UnityEngine;
+
+namespace Dman.GridGameTools.EventLog
 {
     public interface IGridEvent
     {
         public EntityId Entity { get; }
+        public Vector3Int Point { get; }
     }
 }
