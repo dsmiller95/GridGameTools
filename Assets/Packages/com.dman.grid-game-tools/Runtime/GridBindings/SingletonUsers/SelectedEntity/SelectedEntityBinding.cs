25695257f4ce4215403dc43fd10ecaa49cf47369
Dan Miller <dan.miller@fraculation.com>
2024-08-13 11:03:32 -0500

    adjust namespaces

--- Assets/Packages/com.dman.grid-game-tools/Runtime/GridBindings/SingletonUsers/SelectedEntity/SelectedEntityBinding.cs
+++ Assets/Packages/com.dman.grid-game-tools/Runtime/GridBindings/SingletonUsers/SelectedEntity/SelectedEntityBinding.cs
@@ -4,60 +4,63 @@ using Dman.Utilities;
 using JetBrains.Annotations;
 using UnityEngine;
 
-public abstract class SelectedEntityBinding : MonoBehaviour, IRenderUpdate
+namespace Dman.GridGameBindings.SelectedEntity
 {
-    public abstract int RenderPriority { get; }
-    protected ISelectedEntityProvider Select => SingletonLocator<ISelectedEntityProvider>.Instance;
-    private IDungeonUpdater Updater => SingletonLocator<IBindExternalUpdates>.Instance;
+    public abstract class SelectedEntityBinding : MonoBehaviour, IRenderUpdate
+    {
+        public abstract int RenderPriority { get; }
+        protected ISelectedEntityProvider Select => SingletonLocator<ISelectedEntityProvider>.Instance;
+        private IDungeonUpdater Updater => SingletonLocator<IBindExternalUpdates>.Instance;
 
-    /// <summary>
-    /// Flip to true at the beginning of SelectedEntityChanged in order to keep the world waiting.
-    /// Will continue with the world render update after it flips back to false.
-    /// </summary>
-    protected bool KeepWorldUpdatePending { get; set; } 
-    [CanBeNull] protected IDungeonWorld PreviousWorld { get; private set; }
-    protected IDungeonWorld AgainstWorld { get; private set; }
+        /// <summary>
+        /// Flip to true at the beginning of SelectedEntityChanged in order to keep the world waiting.
+        /// Will continue with the world render update after it flips back to false.
+        /// </summary>
+        protected bool KeepWorldUpdatePending { get; set; } 
+        [CanBeNull] protected IDungeonWorld PreviousWorld { get; private set; }
+        protected IDungeonWorld AgainstWorld { get; private set; }
     
-    protected abstract void SelectedEntityChanged([CanBeNull] IDungeonEntity newEntity, bool didIdChange);
+        protected abstract void SelectedEntityChanged([CanBeNull] IDungeonEntity newEntity, bool didIdChange);
     
-    private void OnEnable()
-    {
-        Updater.AddUpdateListener(this);
-        Select.SelectedEntityChanged += OnEntityIdChanged;
-    }
-    private void OnDisable()
-    {
-        Updater?.RemoveUpdateListener(this);
-        if (Select != null)
+        private void OnEnable()
         {
-            Select.SelectedEntityChanged -= OnEntityIdChanged;
+            Updater.AddUpdateListener(this);
+            Select.SelectedEntityChanged += OnEntityIdChanged;
         }
-    }
-
-    private void OnEntityIdChanged(EntityId id)
-    {
-        var world = AgainstWorld ?? DungeonWorldManagerSingleton.Instance.CurrentWorld;
-        var newEntity = world?.EntityStore.GetEntity(id);
-        this.SelectedEntityChanged(newEntity, true);
-    }
-
-    public async UniTask RespondToUpdate(DungeonUpdateEvent update, CancellationToken cancel)
-    {
-        await UniTask.NextFrame(cancel);
-
-        var newEntity = update.NewWorld.EntityStore.GetEntity(Select.SelectedEntity);
-        if (newEntity != null)
+        private void OnDisable()
         {
-            PreviousWorld = update.OldWorld;
-            AgainstWorld = update.NewWorld;
+            Updater?.RemoveUpdateListener(this);
+            if (Select != null)
+            {
+                Select.SelectedEntityChanged -= OnEntityIdChanged;
+            }
         }
-        else
+
+        private void OnEntityIdChanged(EntityId id)
         {
-            PreviousWorld = null;
-            newEntity = update.OldWorld?.EntityStore.GetEntity(Select.SelectedEntity);
-            AgainstWorld = update.OldWorld;
+            var world = AgainstWorld ?? DungeonWorldManagerSingleton.Instance.CurrentWorld;
+            var newEntity = world?.EntityStore.GetEntity(id);
+            this.SelectedEntityChanged(newEntity, true);
         }
-        this.SelectedEntityChanged(newEntity, false);
-        if(KeepWorldUpdatePending) await UniTask.WaitUntil(() => KeepWorldUpdatePending == false, cancellationToken: cancel);
+
+        public async UniTask RespondToUpdate(DungeonUpdateEvent update, CancellationToken cancel)
+        {
+            await UniTask.NextFrame(cancel);
+
+            var newEntity = update.NewWorld.EntityStore.GetEntity(Select.SelectedEntity);
+            if (newEntity != null)
+            {
+                PreviousWorld = update.OldWorld;
+                AgainstWorld = update.NewWorld;
+            }
+            else
+            {
+                PreviousWorld = null;
+                newEntity = update.OldWorld?.EntityStore.GetEntity(Select.SelectedEntity);
+                AgainstWorld = update.OldWorld;
+            }
+            this.SelectedEntityChanged(newEntity, false);
+            if(KeepWorldUpdatePending) await UniTask.WaitUntil(() => KeepWorldUpdatePending == false, cancellationToken: cancel);
+        }
     }
 }
--- Assets/Packages/com.dman.grid-game-tools/Runtime/GridBindings/SingletonUsers/SelectedEntity/SelectedEntityProvider.cs
+++ Assets/Packages/com.dman.grid-game-tools/Runtime/GridBindings/SingletonUsers/SelectedEntity/SelectedEntityProvider.cs
@@ -7,148 +7,151 @@ using Dman.Utilities.Logger;
 using JetBrains.Annotations;
 using UnityEngine;
 
-public interface ISelectedEntityProvider
+namespace Dman.GridGameBindings.SelectedEntity
 {
-    /// <summary>
-    /// Allows setting the selected entity externally, for example, from a mouseclick
-    /// </summary>
-    public EntityId SelectedEntity { get; set; }
-    /// <summary>
-    /// When set to true, will allow the <see cref="SelectedEntity"/> to be changed. When false,
-    /// will cache the last-set value for <see cref="SelectedEntity"/> and apply it after set back to true.
-    /// The last-set value will not be applied instantaneously, it will wait until the next Update
-    /// </summary>
-    /// <remarks>
-    /// Useful when you want to prevent the selected entity from being changed during a specific operation.
-    /// For example, when the user is in the middle of an operation, or when the world is rendering updates from a previous change.
-    /// </remarks>
-    public bool AllowSelectedEntitySwitch { get; set; }
-    /// <summary>
-    /// Emits after the selected entity changes, with the newly selected entity id
-    /// </summary>
-    public event Action<EntityId> SelectedEntityChanged;
-}
-
-/// <summary>
-/// Implement this class and add it to a child of the <see cref="SelectedEntityProvider"/> in order to
-/// filter which entities are allowed to be selected when choosing a default pre-selected entity
-/// </summary>
-public interface IFilterSelectedEntity
-{
-    public bool IncludeEntity(IDungeonEntity entity);
-}
-
-[UnitySingleton]
-public class SelectedEntityProvider : MonoBehaviour, ISelectedEntityProvider, IRenderUpdateAndSwap
-{
-    private IBindExternalUpdates Updater => SingletonLocator<IBindExternalUpdates>.Instance;
-    
-    private EntityId selectedEntity;
-    
-    public EntityId SelectedEntity
+    public interface ISelectedEntityProvider
     {
-        get => selectedEntity;
-        set
+        /// <summary>
+        /// Allows setting the selected entity externally, for example, from a mouseclick
+        /// </summary>
+        public EntityId SelectedEntity { get; set; }
+        /// <summary>
+        /// When set to true, will allow the <see cref="SelectedEntity"/> to be changed. When false,
+        /// will cache the last-set value for <see cref="SelectedEntity"/> and apply it after set back to true.
+        /// The last-set value will not be applied instantaneously, it will wait until the next Update
+        /// </summary>
+        /// <remarks>
+        /// Useful when you want to prevent the selected entity from being changed during a specific operation.
+        /// For example, when the user is in the middle of an operation, or when the world is rendering updates from a previous change.
+        /// </remarks>
+        public bool AllowSelectedEntitySwitch { get; set; }
+        /// <summary>
+        /// Emits after the selected entity changes, with the newly selected entity id
+        /// </summary>
+        public event Action<EntityId> SelectedEntityChanged;
+    }
+
+    /// <summary>
+    /// Implement this class and add it to a child of the <see cref="SelectedEntityProvider"/> in order to
+    /// filter which entities are allowed to be selected when choosing a default pre-selected entity
+    /// </summary>
+    public interface IFilterSelectedEntity
+    {
+        public bool IncludeEntity(IDungeonEntity entity);
+    }
+
+    [UnitySingleton]
+    public class SelectedEntityProvider : MonoBehaviour, ISelectedEntityProvider, IRenderUpdateAndSwap
+    {
+        private IBindExternalUpdates Updater => SingletonLocator<IBindExternalUpdates>.Instance;
+    
+        private EntityId selectedEntity;
+    
+        public EntityId SelectedEntity
         {
-            if (!AllowSelectedEntitySwitch)
+            get => selectedEntity;
+            set
             {
-                Log.Warning("Tried to set selected entity when setting selected entity is disabled");
-                pendingNextSelectedEntity = value;
-                return;
+                if (!AllowSelectedEntitySwitch)
+                {
+                    Log.Warning("Tried to set selected entity when setting selected entity is disabled");
+                    pendingNextSelectedEntity = value;
+                    return;
+                }
+                if (selectedEntity == value) return;
+                selectedEntity = value;
+                SelectedEntityChanged?.Invoke(selectedEntity);
             }
-            if (selectedEntity == value) return;
-            selectedEntity = value;
-            SelectedEntityChanged?.Invoke(selectedEntity);
         }
-    }
 
-    public bool AllowSelectedEntitySwitch
-    {
-        get => _allowSelectedEntitySwitch;
-        set => _allowSelectedEntitySwitch = value;
-    }
-
-    [SerializeField] private bool _allowSelectedEntitySwitch;
-
-    [CanBeNull] private EntityId pendingNextSelectedEntity;
-    public event Action<EntityId> SelectedEntityChanged;
-    
-    private IFilterSelectedEntity[] filters = Array.Empty<IFilterSelectedEntity>();
-
-    private void Awake()
-    {
-        pendingNextSelectedEntity = null;
-    }
-
-    private void OnEnable()
-    {
-        filters = GetComponentsInChildren<IFilterSelectedEntity>();
-        Updater.AddUpdateListenerWithSwap(this);
-    }
-    private void OnDisable()
-    {
-        Updater?.RemoveUpdateListenerWithSwap(this);
-    }
-
-    public int RenderPriority => 0;
-
-    private void Update()
-    {
-        if (pendingNextSelectedEntity != null && AllowSelectedEntitySwitch)
+        public bool AllowSelectedEntitySwitch
         {
-            SelectedEntity = pendingNextSelectedEntity;
+            get => _allowSelectedEntitySwitch;
+            set => _allowSelectedEntitySwitch = value;
+        }
+
+        [SerializeField] private bool _allowSelectedEntitySwitch;
+
+        [CanBeNull] private EntityId pendingNextSelectedEntity;
+        public event Action<EntityId> SelectedEntityChanged;
+    
+        private IFilterSelectedEntity[] filters = Array.Empty<IFilterSelectedEntity>();
+
+        private void Awake()
+        {
             pendingNextSelectedEntity = null;
         }
-    }
 
-    public void OnWorldWasSwapped()
-    {
-        var bestSelectedOption = FindBestSelectedId(DungeonWorldManagerSingleton.Instance.CurrentWorld, selectedEntity, filters);
-        if (bestSelectedOption != selectedEntity)
+        private void OnEnable()
         {
-            Log.Info($"Selected entity changed from {selectedEntity} to {bestSelectedOption}");
-            SelectedEntity = bestSelectedOption;
+            filters = GetComponentsInChildren<IFilterSelectedEntity>();
+            Updater.AddUpdateListenerWithSwap(this);
         }
-    }
-    
-    public UniTask RespondToUpdate(DungeonUpdateEvent update, CancellationToken cancel)
-    {
-        var bestSelectedOption = FindBestSelectedId(update.NewWorld, selectedEntity, filters);
-        if (bestSelectedOption != selectedEntity)
+        private void OnDisable()
         {
-            Log.Info($"Selected entity changed from {selectedEntity} to {bestSelectedOption}");
-            SelectedEntity = bestSelectedOption;
+            Updater?.RemoveUpdateListenerWithSwap(this);
         }
-        return UniTask.CompletedTask;
-    }
 
-    private static EntityId FindBestSelectedId(IDungeonWorld world, EntityId existingId, IFilterSelectedEntity[] filters)
-    {
-        if (world == null)
+        public int RenderPriority => 0;
+
+        private void Update()
         {
-            return existingId;
-        }
-
-        if (existingId != null && world.EntityStore.GetEntity(existingId) != null)
-        { // if the entity exists in the new world, don't try to find a new one.
-            return existingId;
-        }
-
-        var targetEntities = world.EntityStore
-            .AllEntitiesWithIds()
-            .Where(entityPair =>
+            if (pendingNextSelectedEntity != null && AllowSelectedEntitySwitch)
             {
-                return filters.All(x => x.IncludeEntity(entityPair.entity));
-            })
-            .ToList();
-        switch (targetEntities.Count)
-        {
-            case 0:
-                var filterNames = string.Join(", ", filters.Select(x => x.GetType().Name));
-                Log.Error($"SelectedEntityProvider: No selectable entity found, with filters {filterNames}.");
-                return existingId;
+                SelectedEntity = pendingNextSelectedEntity;
+                pendingNextSelectedEntity = null;
+            }
         }
 
-        return targetEntities.First().id;
+        public void OnWorldWasSwapped()
+        {
+            var bestSelectedOption = FindBestSelectedId(DungeonWorldManagerSingleton.Instance.CurrentWorld, selectedEntity, filters);
+            if (bestSelectedOption != selectedEntity)
+            {
+                Log.Info($"Selected entity changed from {selectedEntity} to {bestSelectedOption}");
+                SelectedEntity = bestSelectedOption;
+            }
+        }
+    
+        public UniTask RespondToUpdate(DungeonUpdateEvent update, CancellationToken cancel)
+        {
+            var bestSelectedOption = FindBestSelectedId(update.NewWorld, selectedEntity, filters);
+            if (bestSelectedOption != selectedEntity)
+            {
+                Log.Info($"Selected entity changed from {selectedEntity} to {bestSelectedOption}");
+                SelectedEntity = bestSelectedOption;
+            }
+            return UniTask.CompletedTask;
+        }
+
+        private static EntityId FindBestSelectedId(IDungeonWorld world, EntityId existingId, IFilterSelectedEntity[] filters)
+        {
+            if (world == null)
+            {
+                return existingId;
+            }
+
+            if (existingId != null && world.EntityStore.GetEntity(existingId) != null)
+            { // if the entity exists in the new world, don't try to find a new one.
+                return existingId;
+            }
+
+            var targetEntities = world.EntityStore
+                .AllEntitiesWithIds()
+                .Where(entityPair =>
+                {
+                    return filters.All(x => x.IncludeEntity(entityPair.entity));
+                })
+                .ToList();
+            switch (targetEntities.Count)
+            {
+                case 0:
+                    var filterNames = string.Join(", ", filters.Select(x => x.GetType().Name));
+                    Log.Error($"SelectedEntityProvider: No selectable entity found, with filters {filterNames}.");
+                    return existingId;
+            }
+
+            return targetEntities.First().id;
+        }
     }
 }
