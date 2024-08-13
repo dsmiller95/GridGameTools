0cc42427e5507aeb13cbc306c8dd4543a984f0ec
Dan Miller <dan.miller@fraculation.com>
2024-08-13 10:31:06 -0500

    move the dungeon world loader into grid game tools

---
+++ Assets/Packages/com.dman.grid-game-tools/Runtime/GridBindings/DungeonWorldLoader.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using System.Linq;
+using GridRandom;
+using UnityEngine;
+using WorldCreation;
+
+/// <summary>
+/// Collects all Bindings which are child to this game object.
+/// Will initialize the dungeon world singleton with a world containing these entities.
+/// </summary>
+public class DungeonWorldLoader : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3Int maxBoundsExtension = new Vector3Int(0, 2, 0);
+    
+    private DungeonWorldManager WorldManager => _worldManager ? _worldManager : (_worldManager = GetComponent<DungeonWorldManager>());
+    private DungeonWorldManager _worldManager;
+    
+    public struct EntityGuessAndBinding
+    {
+        public IDungeonEntity entity;
+        public IBoundToEntity binding;
+    }
+
+    private void Start()
+    {
+        var seed = WorldManager.SpawningRng.Fork(nameof(DungeonWorldLoader).ToSeed());
+        var newWorld = CreateAndAddEntitiesFromAllBindings(WorldManager, WorldManager, seed.NextState());
+        WorldManager.TakeInitialWorldState(newWorld);
+    }
+
+    public IDungeonWorld CreateAndAddEntitiesFromAllBindings(
+        IDungeonToWorldContext context,
+        IDungeonUpdater updater,
+        uint seed = 0)
+    {
+        var allBindings = GetComponentsInChildren<IBoundToEntity>();
+        var allPairs = ExtractEntityAndBindingPairs(context, allBindings);
+
+        var bounds = new DungeonBounds(Vector3Int.zero, Vector3Int.one);
+        foreach (EntityGuessAndBinding pair in allPairs)
+        {
+            bounds = bounds.Extend(pair.entity.Coordinate.Position);
+        }
+
+        bounds = bounds.Extend(bounds.Max + maxBoundsExtension);
+
+        var components = GetComponents();
+        var world = DungeonWorld.CreateEmpty(bounds, seed, components);
+
+        (IDungeonWorld newWorld, var entities) = world.AddEntities(allPairs.Select(x => x.entity));
+        var entityIds = entities.ToList();
+        for (int i = 0; i < entityIds.Count; i++)
+        { 
+            var bound = allPairs[i].binding.TryBind(entityIds[i], updater, context, newWorld);
+            if (!bound)
+            {
+                Debug.LogError($"Failed to bind entity {entityIds[i]} to {allPairs[i].binding}");
+            }
+        }
+
+        return newWorld;
+    }
+    
+    public Transform WorldComponentCreators => transform;
+    private List<EntityGuessAndBinding> ExtractEntityAndBindingPairs(IDungeonToWorldContext context, IEnumerable<IBoundToEntity> allBindings)
+    {
+        var entitiesAndBindings = new List<EntityGuessAndBinding>();
+        foreach (IBoundToEntity binding in allBindings)
+        {
+            IDungeonEntity generatedEntity = binding.GetEntityObjectGuessGeneric(context, WorldComponentCreators);
+            if (generatedEntity == null)
+            {
+                Debug.LogError($"Could not guess entity for {binding} of type {binding.GetType()} and name {(binding as UnityEngine.Object)?.name}");
+                continue;
+            }
+            entitiesAndBindings.Add(new EntityGuessAndBinding
+            {
+                entity = generatedEntity,
+                binding = binding,
+            });
+        }
+
+        return entitiesAndBindings;
+    }
+    
+    private IEnumerable<IWorldComponent> GetComponents()
+    {
+        var componentCreators = WorldComponentCreators.GetComponents<ICreateDungeonComponent>();
+        foreach (var creator in componentCreators)
+        {
+            foreach (var component in creator.CreateComponents())
+            {
+                yield return component;
+            }
+        }
+    }
+}
