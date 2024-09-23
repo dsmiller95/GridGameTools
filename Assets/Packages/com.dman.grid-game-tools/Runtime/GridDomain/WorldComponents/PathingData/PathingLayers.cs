e700038a1652410ee4437ffde28fe21561f95271
Dan Miller <dan.miller@fraculation.com>
2024-09-23 12:01:52 -0500

    add user-defined layers to pathing layers enum

--- Assets/Packages/com.dman.grid-game-tools/Runtime/GridDomain/WorldComponents/PathingData/PathingLayers.cs
+++ Assets/Packages/com.dman.grid-game-tools/Runtime/GridDomain/WorldComponents/PathingData/PathingLayers.cs
@@ -1,11 +1,34 @@
+using System;
+
 namespace Dman.GridGameTools.PathingData
 {
-    public enum PathingLayers
+    public enum PathingLayers : int
     {
         Static = 1<<0,
         Mobile = 1<<1,
+        
+        UserLayer00 = 1 << 16,
+        UserLayer01 = 1 << 17,
+        UserLayer02 = 1 << 18,
+        UserLayer03 = 1 << 19,
+        UserLayer04 = 1 << 21,
+        UserLayer05 = 1 << 22,
+        UserLayer06 = 1 << 23,
+        UserLayer07 = 1 << 24,
+        UserLayer08 = 1 << 25,
+        UserLayer09 = 1 << 26,
+        UserLayer10 = 1 << 27,
+        UserLayer11 = 1 << 28,
+        UserLayer12 = 1 << 29,
+        UserLayer13 = 1 << 30,
+        UserLayer14 = 1 << 31,
+        AllUserLayers = UserLayer00 | UserLayer01 | UserLayer02 | UserLayer03 | UserLayer04 | UserLayer05 | UserLayer06 | UserLayer07 | UserLayer08 | UserLayer09 | UserLayer10 | UserLayer11 | UserLayer12 | UserLayer13 | UserLayer14,
     
         None = 0,
-        All = Static | Mobile
+        [Obsolete("Use AllCore or AllLayers, depending on if user-defined layers should be included or not.")]
+        All = Static | Mobile,
+        
+        AllCore = Static | Mobile,
+        AllLayers = AllCore | AllUserLayers,
     }
 }
